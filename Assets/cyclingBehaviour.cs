b003bf819e2080ea354b109fde23f12cea132f37
IrisFolpmers <45753416+IrisFolpmers@users.noreply.github.com>
2023-12-18 16:19:41 +0100

    Button works (kind of)
    Button makes cyclist move

--- Assets/cyclingBehaviour.cs
+++ Assets/cyclingBehaviour.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class cyclingBehaviour : MonoBehaviour
 {
@@ -12,11 +14,22 @@ public class cyclingBehaviour : MonoBehaviour
     public float accelerationFactor = 38f;
     public float turnFactor = 3.5f;
 
+    // public int Spawnbike = 0;
+    public bool Spawn = false;
+    public Button yourButton;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Button btn = yourButton.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
     }
+
+    void TaskOnClick(){
+		Debug.Log ("You have clicked the button!");
+        Spawn = true;
+	}
 
     // Update is called once per frame
     void Update()
@@ -26,10 +39,13 @@ public class cyclingBehaviour : MonoBehaviour
 
     void FixedUpdate()
     {
-        ApplyDriveForce();
-        if(Vector2.Distance(myRigidBody.position, circle.position) <= 5)
-        {
-            myRigidBody.AddForce(new Vector2(0,1));
+        // Spawnbike = !Spawnbike;
+        if(Spawn){
+            ApplyDriveForce();
+            if(Vector2.Distance(myRigidBody.position, circle.position) <= 5)
+            {
+                myRigidBody.AddForce(new Vector2(0,1));
+            }
         }
     }
 
@@ -39,4 +55,6 @@ public class cyclingBehaviour : MonoBehaviour
         myRigidBody.MoveRotation(rotationAngle);
         myRigidBody.AddForce(new Vector2(1,0));
     }
+
+    
 }
