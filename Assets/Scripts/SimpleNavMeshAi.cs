305a5df474e12b9db960fcc34bacfbfaa399a327
ChromaChroma <jona.beer@gmail.com>
2023-12-20 17:38:00 +0100

    Add despawn script for bicycle agents

--- Assets/Scripts/SimpleNavMeshAi.cs
+++ Assets/Scripts/SimpleNavMeshAi.cs
@@ -9,10 +9,13 @@ public class SimpleNavMeshAi : MonoBehaviour
     public Transform goal;
 
     private NavMeshAgent _agent;
+    private MeshRenderer _meshRenderer;
+    private float _goalLocationOffset;
 
     private void Awake()
     {
         _agent = GetComponent<NavMeshAgent>();
+        _meshRenderer = gameObject.GetComponent<MeshRenderer>();
     }
 
     private void Start()
@@ -20,6 +23,17 @@ public class SimpleNavMeshAi : MonoBehaviour
         if (goal is not null) // Not sure this always runs After adding goal when Instantiating
         {
             _agent.destination = goal.position;
+            var bounds = goal.GetComponent<MeshRenderer>().bounds;
+            _goalLocationOffset = Math.Min(bounds.size.x,bounds.size.z);
+        }
+    }
+
+    private void Update()
+    {
+        var goalPos = goal.transform.position;
+        if (Vector3.Distance(goalPos, transform.position) <= _goalLocationOffset )
+        {
+            Destroy(gameObject);
         }
     }
 }
