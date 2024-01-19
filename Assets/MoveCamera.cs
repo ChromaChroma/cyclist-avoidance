e79628f41003ada08f6dee901fe5c2f2024e35bd
BorisDeGraaff <borisdegraaff@gmail.com>
2024-01-19 12:48:14 +0100

    Camera presets (num 1, 2 and 3) + movement
    Use the number keys to quickly change the camera position to preset location defined in MoveCamera.cs

--- Assets/MoveCamera.cs
+++ Assets/MoveCamera.cs
@@ -22,17 +22,30 @@ public class MoveCamera : MonoBehaviour
         if(inputZ != 0) {
             move();
         }
+    if ( Input.GetKeyDown(KeyCode.Alpha1) == true ){
+        transform.position = new Vector3(0,50,3);
+        transform.rotation = Quaternion.Euler(90f, 0f, 0f);
+    }
+    if ( Input.GetKeyDown(KeyCode.Alpha2) == true ){
+        transform.position = new Vector3(25,40,-5);
+        transform.rotation = Quaternion.Euler(60f, 290f, 0f);
+    }
+    if ( Input.GetKeyDown(KeyCode.Alpha3) == true ){
+        transform.position = new Vector3(14,2,-3);
+        transform.rotation = Quaternion.Euler(0f, 300f, 0f);
+    }
+
 
     }
 
     private void rotate()
     {
-        transform.Rotate(new Vector3(0f, inputX * Time.deltaTime, 0f));
+        transform.Rotate(new Vector3(0f, inputX * Time.deltaTime * 2, 0f));
     }
 
     private void move()
     {
-        transform.position += transform.forward * inputZ * Time.deltaTime;
+        transform.position += transform.forward * inputZ * Time.deltaTime * 2;
     }
 
 
