6a883ab0918a422520f074656f1889c440cadf2a
Sembodi <aapje11g@gmail.com>
2024-01-22 14:56:16 +0100

    fietserScript dat remt bij fietsers van rechts toegevoegd

--- Assets/FietserCode.cs
+++ Assets/FietserCode.cs
@@ -34,17 +34,43 @@ public class FietserController : MonoBehaviour
         if (agent.transform.position == endLoc)
             agent.SetDestination(startLoc);
 
-        //dom idee:
-        //if ((agent.transform.position - other.transform.position).magnitude <= 20)
-        //{
-        //    var rotation = Quaternion.AngleAxis(1, Vector3.up);
+        //idee:
+        if ((agent.transform.position - other.transform.position).magnitude <= 20)
+        {
 
-        //    Vector3 fiets1V = agent.velocity;
-        //    Vector3 fiets2V = other.velocity;
+            Vector3 fiets1V = agent.velocity;
+            Vector3 fiets2V = other.velocity;
 
-        //    agent.velocity = Vector3.RotateTowards(fiets1V,fiets2V,(float)Math.PI/360,10);//rotation * fiets1V;
-        //    other.velocity = Vector3.RotateTowards(fiets2V,fiets1V,(float)Math.PI/360,10);//rotation * fiets2V;
-        //}
+            Vector3 rotateRight = Vector3.Cross(fiets1V,Vector3.up);
+            Vector3 sForce; //steering force
+            Vector3 bForce; //braking force
+
+
+            if (Vector3.Dot(fiets2V-fiets1V, rotateRight) > 0) //if the distance vector between the two cyclists is in the same direction as the velocity turned 90 degrees to the right
+            {
+                bForce = -fiets1V * .1f;
+            }
+            else
+            {
+                bForce = Vector3.zero;
+            }
+            
+
+            agent.velocity = agent.velocity + bForce;//rotation * fiets1V;
+
+            rotateRight = Vector3.Cross(fiets2V, Vector3.up);
+
+            if (Vector3.Dot(fiets1V - fiets2V, rotateRight) > 0) //if the distance vector between the two cyclists is in the same direction as the velocity turned 90 degrees to the right
+            {
+                bForce = -fiets2V * .1f;
+            }
+            else
+            {
+                bForce = Vector3.zero;
+            }
+
+            other.velocity = other.velocity + bForce;//rotation * fiets2V;
+        }
     }
 
 
