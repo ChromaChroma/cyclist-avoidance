cfaebf50ad25c5da0fcd83428ed519fac735d598
Sembodi <aapje11g@gmail.com>
2024-01-24 22:34:38 +0100

    fietsers blijven nu rechts fietsen

--- Assets/Scripts/SimpleNavMeshAi.cs
+++ Assets/Scripts/SimpleNavMeshAi.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.AI.Navigation;
@@ -11,6 +12,7 @@ public class SimpleNavMeshAi : MonoBehaviour
 
     private NavMeshAgent _agent;
     private float _goalLocationOffset;
+    private List<Vector3> _destinations = new List<Vector3>();
 
     private void Awake()
     {
@@ -21,7 +23,24 @@ public class SimpleNavMeshAi : MonoBehaviour
     {
         if (goal is not null) // Not sure this always runs After adding goal when Instantiating
         {
-            _agent.destination = goal.position;
+            var distZroad = Math.Abs(_agent.transform.position.x - 0.29); //distance from Z-road
+            var distXroad = Math.Abs(_agent.transform.position.z - 1.25); //distance from X-road
+
+            if (distXroad < distZroad)
+            {
+                _destinations.Add(new Vector3(goal.transform.position.x, goal.transform.position.y, _agent.transform.position.z));
+            }
+            else
+            {
+                _destinations.Add(new Vector3(_agent.transform.position.x, goal.transform.position.y, goal.transform.position.z));
+            }
+
+            _destinations.Add(goal.transform.position);
+
+
+            //_agent.agentTypeID = 1;
+            _agent.radius = 0.5f;
+            _agent.destination = _destinations[0];
             var bounds = goal.GetComponent<MeshRenderer>().bounds;
             _goalLocationOffset = Math.Min(bounds.size.x,bounds.size.z);
         }
@@ -29,9 +48,14 @@ public class SimpleNavMeshAi : MonoBehaviour
 
     private void Update()
     {
-        if (Vector3.Distance(goal.transform.position, transform.position) <= _goalLocationOffset )
+        if (Vector3.Distance(_agent.destination, _agent.transform.position) < _goalLocationOffset )
         {
-            Destroy(gameObject);
+            if (_destinations.Count == 1) Destroy(gameObject);
+            else
+            {
+                _destinations.RemoveAt(0);
+                _agent.SetDestination(_destinations[0]);
+            }
         }
     }
 }
